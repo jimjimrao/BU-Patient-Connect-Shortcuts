8713464a48e71747f67e181238542277d12873b0
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2021-02-28 01:51:17 -0500

    removed keystrokes and utilizes tab switching and sending keys

--- main.py
+++ main.py
@@ -1,35 +1,53 @@
-
+#!/usr/bin/env python
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time, pyautogui
+
 
 username = input("Enter your BU username:")
 from getpass import getpass
 pw = getpass("Enter your password:")
 
+
+
+start = time.time()
 web = webdriver.Chrome()
 web.get('https://www.bu.edu/shs/getting-started/using-patient-connect/')
+wait = WebDriverWait(web,10)
 
-secure_login_button = web.find_element_by_xpath('//*[@id="post-7513"]/p[2]/a')
-secure_login_button.click() 
+wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="post-7513"]/p[2]/a'))).click()
 
-#find a way to switch tabs 
+#switch tabs 
+web.switch_to.window(web.window_handles[1])
 
 #Log in Screen
-time.sleep(2)
-pyautogui.typewrite(username + '\t' + pw + '\t')  #NOTE need to change this so that it doesn't not use key strokes 
-pyautogui.press('enter')
+username_input = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="j_username"]')))
+username_input.send_keys(username)
+
+pw_input = web.find_element_by_xpath('//*[@id="j_password"]')
+pw_input.send_keys(pw)
+
+login_button = web.find_element_by_xpath('/html/body/div[1]/div/form/button')
+login_button.click()
+
 print("Logging in...")
 
 
-
-web.get('https://patientconnect.bu.edu/Mvc/Patients/QuarantineSurvey')
-pyautogui.hotkey('optionleft','command','left')                     #NOTE need to change this so that it doesn't not use key strokes 
-pyautogui.press('enter')
-survey_button = web.find_element_by_xpath('//*[@id="mainbody"]/div[2]/div[1]/div/div[2]/a')
+#Click Survey Button
+survey_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ctl03"]/div[3]/div/a')))
 survey_button.click()
-time.sleep(1)
+
+#Click Continue Button
+time.sleep(0.5)
+continue_button = web.find_element_by_xpath('//*[@id="mainbody"]/div[2]/div[1]/div/div[2]/a')
+continue_button.click()
+
+
+time.sleep(0.5)
 #Survey Questions
 web.find_element_by_xpath('//*[@id="mainbody"]/main/form/div[2]/fieldset/div/div[1]/div').click()
 web.find_element_by_xpath('//*[@id="mainbody"]/main/form/div[3]/fieldset/div/div[1]/div').click()
@@ -39,10 +57,11 @@ web.find_element_by_xpath('//*[@id="mainbody"]/main/form/div[6]/fieldset/div/div
 web.find_element_by_xpath('//*[@id="mainbody"]/main/form/div[7]/fieldset/div/div[1]/div').click()
 web.find_element_by_xpath('//*[@id="mainbody"]/main/form/div[8]/fieldset/div/div[1]/div').click()
 web.find_element_by_xpath('//*[@id="mainbody"]/main/form/div[9]/fieldset/div/div[1]/div').click()
+print('Survey Complete')
 #Submit Survey
 web.find_element_by_xpath('//*[@id="mainbody"]/footer/div/div[2]/input').click()
-
 print('Survey Submitted')
+print('This took: %.2f seconds' % (time.time()-start))
 
 
 
